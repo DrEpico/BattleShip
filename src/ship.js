53a7f2a32bdd188bf8c96ebced7006db8c3f5eb1
ali <adadashipour@gmail.com>
2024-08-16 16:26:11 +0100

    Add setPositions() and property to track ship position

--- src/ship.js
+++ src/ship.js
@@ -3,6 +3,12 @@ export class Ship{
         this.length = length;
         this.numOfHits = 0;
         this.isSunk = false;
+        this.positions = []; // Array to store positions occupied by the ship
+    }
+
+    // Add a method to set positions when the ship is placed
+    setPositions(positions) {
+        this.positions = positions;
     }
 
     hit(){
