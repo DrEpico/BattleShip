ec31c53f1b2a83464304ccc4c71d0996ca8bbb47
ali <adadashipour@gmail.com>
2024-08-17 13:08:05 +0100

    Add basic game driver logic and styling

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 import { Player } from "./player";
 import { game } from "./driver";
 
+domModule.renderHomeScreen();
+
 game();
+
+domModule.updateBoardUI(player1.gameboard, 'player1');
+domModule.updateBoardUI(player2.gameboard, 'player2');
+
+// Example: Bind click events to Player 1's board for attacks
+domModule.bindBoardClickEvents('player1', (x, y) => {
+    player2.gameboard.receiveAttack(x, y);
+    domModule.updateBoardUI(player2.gameboard, 'player2');
+});
