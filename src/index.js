3f07774887c87ae72a32fc69c7670223458ad2d1
ali <adadashipour@gmail.com>
2024-08-18 18:15:59 +0100

    Refactor player board initialisation

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import { domModule } from './domModule';
 import { Player } from './player';
+import { Ship } from './ship';
 import './style.css';
 
 domModule.renderHomeScreen();
 
-const player1 = new Player('human');
-const player2 = new Player('computer');
+const player = new Player('human');
+// populate player’s Gameboard with predetermined coordinates.
+player.gameboard.placeShip(new Ship(5), 2,2, 'right');
+player.gameboard.placeShip(new Ship(4), 5,4, 'down');
+player.gameboard.placeShip(new Ship(3), 8,7, 'right');
+player.gameboard.placeShip(new Ship(3), 0,0, 'down');
+player.gameboard.placeShip(new Ship(2), 6,6, 'down');
 
-domModule.updateBoardUI(player1.gameboard, 'player1');
-domModule.updateBoardUI(player2.gameboard, 'player2');
+const computer = new Player('computer');
+
+domModule.updateBoardUI(player.gameboard, 'player1', true);
+domModule.updateBoardUI(computer.gameboard, 'player2',);
 
 // Example: Bind click events to Player 1's board for attacks
-domModule.bindBoardClickEvents('player1', (x, y) => {
-    player2.gameboard.receiveAttack(x, y);
-    domModule.updateBoardUI(player2.gameboard, 'player2');
+domModule.bindBoardClickEvents('player2', (x, y) => {
+    computer.gameboard.receiveAttack(x, y);
+    domModule.updateBoardUI(computer.gameboard, 'player2');
 });
