a279cb6c75ebb9bf86941d42c46c94e551dbc8f0
ali <adadashipour@gmail.com>
2024-08-21 17:35:01 +0100

    Add some initial logic for user determined ship placement

--- src/gameController.js
+++ src/gameController.js
@@ -7,26 +7,69 @@ export class GameController {
         this.humanPlayer = new Player('human');
         this.computerPlayer = new Player('computer');
         this.currentPlayer = this.humanPlayer; // Human starts first
+        this.shipsToPlace = [5, 4, 3, 3, 2]; // Ship lengths to be placed
+        this.currentShipIndex = 0; // Track which ship is being placed
     }
 
-    // Method to start the game
     startGame() {
         domModule.renderHomeScreen();
-        //TODO: players to decide their ship positions
-        this.humanPlayer.gameboard.placeShip(new Ship(5), 2,2, 'right');
-        this.humanPlayer.gameboard.placeShip(new Ship(4), 5,4, 'down');
-        this.humanPlayer.gameboard.placeShip(new Ship(3), 8,7, 'right');
-        this.humanPlayer.gameboard.placeShip(new Ship(3), 0,0, 'down');
-        this.humanPlayer.gameboard.placeShip(new Ship(2), 6,6, 'down');
-        console.log("game starts");
-        this.updateUI();
+        this.promptShipPlacement();
+    }
 
+    promptShipPlacement() {
+        const currentShipLength = this.shipsToPlace[this.currentShipIndex];
+        alert(`Place your ship of length ${currentShipLength}. Click on the board to place it, and choose a direction.`);
 
-        // Bind click events for the human player to attack the computer's board
+        // Bind the click event for ship placement
+        domModule.bindBoardClickEvents('player1', (x, y) => {
+            this.handleShipPlacement(x, y, currentShipLength);
+        });
+    }
+
+    handleShipPlacement(x, y, length) {
+        const direction = prompt("Enter the direction to place the ship (right/down):").toLowerCase();
+        try {
+            this.humanPlayer.gameboard.placeShip(new Ship(length), parseInt(x), parseInt(y), direction);
+            this.updateUI();
+            this.currentShipIndex++;
+
+            if (this.currentShipIndex < this.shipsToPlace.length) {
+                this.promptShipPlacement();
+            } else {
+                this.startCombatPhase();
+            }
+        } catch (error) {
+            alert(error.message);
+        }
+    }
+
+    startCombatPhase() {
+        alert("All ships placed! The game begins now.");
+        // Allow the user to attack the computer's board
         domModule.bindBoardClickEvents('player2', (x, y) => {
             this.humanTurn(x, y);
         });
+        this.updateUI();
     }
+
+    // // Method to start the game
+    // startGame() {
+    //     domModule.renderHomeScreen();
+    //     //TODO: players to decide their ship positions
+    //     this.humanPlayer.gameboard.placeShip(new Ship(5), 2,2, 'right');
+    //     this.humanPlayer.gameboard.placeShip(new Ship(4), 5,4, 'down');
+    //     this.humanPlayer.gameboard.placeShip(new Ship(3), 8,7, 'right');
+    //     this.humanPlayer.gameboard.placeShip(new Ship(3), 0,0, 'down');
+    //     this.humanPlayer.gameboard.placeShip(new Ship(2), 6,6, 'down');
+    //     console.log("game starts");
+    //     this.updateUI();
+
+
+    //     // Bind click events for the human player to attack the computer's board
+    //     domModule.bindBoardClickEvents('player2', (x, y) => {
+    //         this.humanTurn(x, y);
+    //     });
+    // }
 
     // Human player's turn
     humanTurn(x, y) {
